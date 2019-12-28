540936ee8b42f7208f02f3799d7075998ad8b19d
whitetower <1065635703@qq.com>
2019-12-28 20:36:28 +0800

    函数参数和返回值示例â

---
+++ functions_demo/function_args.go
@@ -0,0 +1,11 @@
+package functions_demo
+
+import "fmt"
+
+/*
+函数可以接收0或多个参数
+当连续两个或多个函数的已命名形参类型相同时，除最后一个类型以外，其它都可以省略
+*/
+func funcArgs(x, y int) {
+	fmt.Println(x + y)
+}
---
+++ functions_demo/function_return.go
@@ -0,0 +1,14 @@
+package functions_demo
+
+/*
+函数可以返回任意数量的返回值
+Go 的返回值可被命名，它们会被视作定义在函数顶部的变量
+没有参数的 return 语句返回已命名的返回值。也就是 直接 返回
+Java不能直接返回任意数量的返回值，而是将多个返回值进行封装，再返回一个返回值
+*/
+func funcReturn() (cls string, num int, stu string) {
+	cls = "班上有"
+	num = 6
+	stu = "名同学"
+	return
+}
